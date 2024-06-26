efc38351be8b37922bda87621c0b3af4b0d5f3c0
Mayur-9890 <158853348+Mayur-9890@users.noreply.github.com>
2024-06-26 22:39:37 +0530

    -Initial Commit -Implemetation base architecture -Created Login web API

---
+++ TalentoTrack.Service/AccountServices.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentoTrack.Common.DTOs.Account;
+using TalentoTrack.Common.Repoitories;
+using TalentoTrack.Common.Services;
+
+namespace TalentoTrack.Service
+{
+    public class AccountServices : IAccountServices
+    {
+        public readonly IAccountRepoitories _accountRepoitories;
+
+        public AccountServices(IAccountRepoitories accountRepoitories)
+        {
+            _accountRepoitories = accountRepoitories;
+        }
+        public async Task<LoginResponse> VerifyLoginDetails(LoginRequest request)
+        {
+            LoginResponse response = new LoginResponse();
+            try
+            {
+                var dbUser = await _accountRepoitories.GetLoginDetails(request.Username!, request.Password!);
+                if (dbUser == null)
+                {
+                    response.Success = false;
+                    response.ErrorMessage = "Invalide Credentials";
+                }
+                else
+                {
+                    response.Success = true;
+                }
+                return response;
+            }
+            catch (Exception ex)
+            {
+                //Todo: Log error
+                throw;
+            }
+        }
+    }
+}
